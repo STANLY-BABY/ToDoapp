3b16abadce59b4737fcd94dc2fd1fbce2abb361a
Stanly Baby <mailstanlybaby@gmail.com>
2022-12-06 10:12:52 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {useState} from "react";
 function App() {
+  const [toDos,setToDos] = useState([])
+  const [toDo,setToDo] = useState('')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="checklist">
+  <h2>To Do List </h2>
+  <div className='input'>
+    <input className='input-field' value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder='Add items ....' />
+    <i onClick={()=>setToDos([...toDos,{id:Date.now(), text: toDo,status:false}])} className='fas fa-plus icon'></i>
+ 
+  </div>
+  {toDos.map((obj)=>{
+   return( 
+   <label>
+    <input onChange={(e)=>{
+      setToDos(toDos.filter(obj2=>{
+        if(obj2.id===obj.id){
+          obj2.status=e.target.checked
+        }
+        return obj2
+      }))
+
+    }} value={obj.status} type="checkbox" name="" id="" />
+    <span>{obj.text}     </span>
+     <i onClick={()=>setToDos([])} className="fa-solid fa-xmark" ></i>
+  </label>)
+  })}
+
+    <h2>ACTIVE TASK</h2>
+    <div>
+  {toDos.map((obj)=>{
+    if (obj.status) {
+      return(<h2>{obj.text}</h2>)
+    }
+    return null
+  })}
+  </div>
+</div>
     </div>
   );
 }
